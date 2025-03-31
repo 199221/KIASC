9ed5e353d76cfc63d3646d306e8d61cb1c7b1b0b
SIVA <141731242+199221@users.noreply.github.com>
2025-03-31 16:51:40 +0530

    Update settings.py

--- website_project/settings.py
+++ website_project/settings.py
@@ -75,17 +75,25 @@ WSGI_APPLICATION = 'website_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'web_site',
-        'USER':'root',
-        'PASSWORD':'KAMAL@2003nath',
-        'HOST':'127.0.0.1',
-        'PORT':'3306'
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'web_site',
+#         'USER':'root',
+#         'PASSWORD':'KAMAL@2003nath',
+#         'HOST':'127.0.0.1',
+#         'PORT':'3306'
+#     }
 
+# }
+
+import dj_database_url
+import os
+
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
+
 
 
 # Password validation
